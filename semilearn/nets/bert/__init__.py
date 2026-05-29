91833ad82f62b2ffb0a0da0acfcf3443e92d74e4
Mehrab Mustafy Rahman <nibir1705@gmail.com>
2026-05-28 20:51:22 -0500

    Add files via upload

---
+++ semilearn/nets/bert/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+from .bert import bert_base_cased, bert_base_uncased
+# from .bert import ClassificationBert
+from .bert_multihead import bert_base_cased_multihead, bert_base_uncased_multihead
